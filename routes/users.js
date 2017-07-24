755d1cd84635df84d08ee6a30276c9eba3a3918e
Mark Stanford <m.stanford@samsung.com>
2017-07-24 11:28:59 -0700

    Removed req body from users api calls

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.route('/login')
 	models.user.findOne({email: req.body.email}, function(error, userFound){
 		if(error){
 			//Send error
-			res.status('400').send(templates.response(codes.fail, "fail", "Error retrieving user", req.body));
+			res.status('400').send(templates.response(codes.fail, "fail", "Error retrieving user", {}));
 		}else{
 			if(userFound){
 				var hash = utils.createHash(req.body.password);
@@ -32,18 +32,18 @@ router.route('/login')
 					var sessionModel = models.session({user:userFound._id, token:sesstoken});
 					sessionModel.save(function(error, session){
 						if(error){
-							res.status('400').send(templates.response(codes.fail, "fail", "Error creating session token", req.body));
+							res.status('400').send(templates.response(codes.fail, "fail", "Error creating session token"));
 						}else{
-							res.send(templates.response(codes.success, "success", session, req.body));		
+							res.send(templates.response(codes.success, "success", {session:session, user:userFound}));		
 						}
 					});
 				}else{
 					//Bad password
-					res.status('400').send(templates.response(codes.bad_password, "fail", "Password incorrect.", req.body));
+					res.status('400').send(templates.response(codes.bad_password, "fail", "Password incorrect."));
 				}
 			}else{
 				//No User found
-				res.status('400').send(templates.response(codes.no_user_found, "fail", "User not found.", req.body));
+				res.status('400').send(templates.response(codes.no_user_found, "fail", "User not found."));
 			}
 		}
 	});
@@ -53,10 +53,10 @@ router.route('/logout').post(function(req, res){
 	models.session.remove({token: req.body.token}, function(error){
 		if(error){
 			//Send Error.
-			res.status('400').send(templates.response(codes.fail, "fail", "Error logging out user.", req.body));
+			res.status('400').send(templates.response(codes.fail, "fail", "Error logging out user."));
 		}else{
 			//Send Success.
-			res.send(templates.response(codes.success, "success", words, req.body));		
+			res.send(templates.response(codes.success, "success", words));		
 		}
 	});
 });
@@ -73,10 +73,10 @@ router.route('/register').post(function(req, res){
 				message = "Email already registered";
 				code = codes.duplicate_username;
 			}
-			res.status('400').send(templates.response(code, "fail", message, req.body));
+			res.status('400').send(templates.response(code, "fail", message));
 		}else{
 			//send success.
-			res.send(templates.response(codes.success, "success", user, req.body));		
+			res.send(templates.response(codes.success, "success", user));		
 		}
 	});
 });
